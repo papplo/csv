b2d50135b781d2cea10cb3c1813d887de8edc302
Pablo Anttila <pablo@queenslab.se>
2022-12-27 00:02:57 +0100

    Make parser a bit more modular in code

--- index.js
+++ index.js
@@ -1,42 +1,96 @@
-const fs = require('fs');
-const readline = require("readline");
+import { argv } from 'node:process';
+import { createInterface } from "readline";
+import { createReadStream, mkdirSync, writeFileSync, readFileSync } from 'fs';
 
 // Init
-const stream = fs.createReadStream('./sample.csv');
-const rl = readline.createInterface( { input: stream });
+if (!argv[2]) {
+    console.log('Please provide a csv file to parse');
+    process.exit(0);
+}
 
 let data = [];
-rl.on("line", (row) => {
-    data.push(row.split(","));
-});
+let rl;
+const filename = argv[2];
+try {
+    const stream = createReadStream(`./${filename}`);
+    rl = createInterface({ input: stream });
+} catch (error) {
+    throw new Error(error);
+}
 
-rl.on("close", async () => {
-    console.clear()
+rl.on("line", readLineToDataset);
+
+rl.on("close", parseCallback);
+
+function parseCallback() {
+    console.clear();
     console.log(`Done! Parsed file of ${data.length} lines`);
-    const { array, data: calc, rest} = parseToAverage(data);
 
-    console.log( array, calc ,rest );
+    if (data.length > 1) {
+        let parsedResult = parse(data);
+        let res = write('/output/', 'parsed', serializeToCsv(parsedResult));
+        console.log(`Writing file to ${res}`);
+    } 
+    process.exit()
+}
 
+function readLineToDataset(line) { 
+    data.push(line.split(','))
+};
 
-})
+function parse(dataSet) {
+    const result = dataSet.reduce((prev, curr, array) => {
+        const [dateTime, temperature] = curr;
+        const [date, time] = dateTime.split("T");
 
+        if (!prev.has(date)) {
+            prev.set(date, Array.from([temperature]))
+        } else {
+            const previousTemperatures = prev.get(date);
+            prev.set(date, [...previousTemperatures, temperature])
+        }
+        return prev
 
- function parseToAverage(array) {
-    let data = [];
-    let res = [];
-    let index = 1;
+    }, new Map());
 
-    data.push(array[0][0].split("T")[0]);
-    res.push(array[0][1]);
+    return Array.from(result.entries()).map(([day, temps]) => {
+        let sumTemps = temps.reduce((p, c) => Number(p) + Number(c), 0);
+        let avgTemp = 0 + sumTemps / temps.length;
+        let shortFormat = Number.parseFloat(avgTemp).toFixed(1);
+        return [day, shortFormat]
+    })
+};
 
-    while (array[index][0].split("T")[0] === array[index -1][0].split("T")[0]) {
-        res.push(array[index][1])
-        index++;
-    }
-    data.push(res);
-  
-    return { array, data, rest: index }
+function serializeToCsv(data) {
+    return data.join('\n')
+};
+
+function write(dirPath, fileEnding, writeableData) {
+    // create path
+    const path = process.cwd() + dirPath;
+    let processedFilename = [filename.split('.')[0], fileEnding].concat(filename.split('.')[1]).join('.')
+    
+    // create dir
+    mkdirSync(path, { recursive: true }, (error) => {
+        if (error) {
+            console.error('An error ocurred creating output path:', error);
+        }
+    });
     
 
+    // check if file exists
+    const currentFile = readFileSync(`${path}/${processedFilename}`);
+    if (currentFile) {
+        console.log('A file exists with name:', processedFilename);
+    }
 
+    // create file and write data
+    writeFileSync(`${path}/${processedFilename}`, writeableData, (error) => {
+        if (error) {
+            console.error('an error ocurred writing your file:', error);
+        }
+    })
+
+    return [path, processedFilename].join('');
+    
 }
