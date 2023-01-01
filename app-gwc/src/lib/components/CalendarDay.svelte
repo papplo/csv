9a21902c0924863251d7dae91b1eea52eb9f3700
Pablo Anttila <pablo@queenslab.se>
2023-01-01 23:21:00 +0100

    move components to /lib

---
+++ app-gwc/src/lib/components/CalendarDay.svelte
@@ -0,0 +1,27 @@
+<script>
+    /**
+	 * @type {string | number | Date}
+	 */
+     export let day
+
+     const calendarDay = new Date(day);
+     const isSunday = calendarDay.getDay() === 0
+
+</script>
+
+<li class={isSunday ? 'isSunday' : 'isWeekday'}>
+    {calendarDay.getDate()}
+</li>
+
+<style>
+    li {
+      font-weight: 700;
+      flex-grow: 1;
+      list-style: none;
+      aspect-ratio: 2 / 1.5;
+      border: 1px solid #000;
+    }
+    li.isSunday {
+        background-color: #000;
+    }
+</style>
